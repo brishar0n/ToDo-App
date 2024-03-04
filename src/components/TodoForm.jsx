a8590f26653f31f365c0e4df06198d0821d827f6
Brigitte <114067290+brishar0n@users.noreply.github.com>
2024-03-05 00:04:24 +0700

    Update TodoForm.jsx

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function TodoForm({ addTodo }) {
+export function TodoForm({ addTodo, filterTodo, setFilter }) {
   const [newItem, setNewItem] = useState("");
 
   function handleSubmit(e) {
@@ -16,18 +16,27 @@ export function TodoForm({ addTodo }) {
     <div>
       <form>
         <div>
-          <label class="font-semibold" htmlFor="item">Enter a new task: </label>
+          <label class="font-sans font-semibold tracking-widest" htmlFor="item">Enter a new task: </label>
           <input
             type="text"
             id="item"
             value={newItem}
             onChange={(e) => setNewItem(e.target.value)}
-            className="w-full"
+            className="w-full rounded-lg mt-1"
           />
         </div>
         <button class="bg-lime-900 hover:bg-lime-800 text-white font-medium w-full py-1 px-4 my-5 rounded" onClick={handleSubmit}>
           Add
         </button>
+
+        <div className="mt-0.3 mb-3">
+          <select onChange={(e) => setFilter(e.target.value)} className="bg-neutral-400 text-black w-fit p-1 rounded">
+            <option value="All">All</option>
+            <option value="Active">Active</option>
+            <option value="Completed">Completed</option>
+          </select>
+        </div>
+
       </form>
     </div>
   );
